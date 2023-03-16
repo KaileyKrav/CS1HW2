4ba4c3cb1b01a18000652e7e6ffd0cba06d37174
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-03-16 15:46:26 -0400

    Create Q2.py

---
+++ Q2.py
@@ -0,0 +1,15 @@
+#Write a program to calculate the total of a grocery list. 
+#Create 5 variables to represent the items in the list and their prices. 
+#Add up all the prices and print out the entire shopping list and total.
+#Your output should look like this:
+#    Reciept:
+#    item 1: Eggs - 3.99
+#    item 2: Milk - 2.50
+#    ....
+#    Total:        10.65
+
+#Instead of creating 5 variables, put the items into a list and print it out.
+#Again, add up the items (EX: total = thisList[1] + thisList[3])
+#The user decided to put an item back. 
+#Ask the user which item they would like to remove and remove the item and its price from the list
+#Now reprint out the new list. 
