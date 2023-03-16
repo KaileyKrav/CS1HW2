ab49a900851588f12b5a9109fba8a3a8d4b964b8
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-03-16 15:39:27 -0400

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,8 @@
+#Write a program that asks the user for their first and last name and their birthday. 
+#Calculate their current age from their birthday and print out the following:
+#Full Name:_______
+#Birthday: _______
+#Age: _______
+
+#Now using if...else statements determine which of the 12 zodiac signs the user is. (You will have to use elifs as well)
+#For example: if the user's birthday falls between August 23rd & September 22nd, print out that they are a Virgo.
